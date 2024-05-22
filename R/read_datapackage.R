2e99e56e52bfa6909cfc11127611c3df60c5c78a
Henrique de Souza Linhares <43452663+hslinhares@users.noreply.github.com>
2024-05-22 12:34:11 -0300

    Cria estrutura básica do pacote `dpm-r`. (#3)
    * Cria estrutura básica do pacote
    
    * insere a função is_url no utils.R
    
    * ajusta nome do pacote
    
    * rename Rproj to match package name
    
    * add .Rhistory to gitignore
    
    * adiciona pacotes here e data.table como dependencies em Imports no DESCRIPTION
    
    * ajusta nome dos autores do pacote
    
    * stop tracking and ignore CreatingPackage.R
    
    ---------
    
    Co-authored-by: Francisco Júnior <fjunior.alves.oliveira@gmail.com>

---
+++ R/read_datapackage.R
@@ -0,0 +1,18 @@
+#´ Criar uma lista de dataframes
+#'
+#' Faz a leitura de recursos de um datapackage e cria um lista de daframes
+#' @param path Arquivo datapackage
+#' @returns Uma lista de dataframes
+#' @export
+read_datapackage <- function(path) {
+  if (!is_url(path)) {
+    path <- here::here(path)
+  }
+  package <- suppressMessages(frictionless::read_package(path))
+  resource_names <- frictionless::resources(package)
+  result <- lapply(resource_names, function(resource_name) {
+    data.table::as.data.table(frictionless::read_resource(package, resource_name))
+  })
+  names(result) <- resource_names
+  result
+}
